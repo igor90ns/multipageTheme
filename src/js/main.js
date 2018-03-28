3fee275a63b9f1243ba6db22a4f3c8e6756fde69
igor90ns <aleksic90@gmail.com>
2018-03-28 22:54:59 +0200

    added slick slider.js and testemonials

--- src/js/main.js
+++ src/js/main.js
@@ -15,4 +15,10 @@ $(function() {
 $(document).on('click','[data-toggle="lightbox"]', function(event){
   event.preventDefault();
   $(this).ekkoLightbox();
-})
+});
+
+$('.slider').slick({
+  infinite:true,
+  slideToShow:1,
+  slideToScroll:1
+});
